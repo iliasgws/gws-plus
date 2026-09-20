b2d90df8e7b5cd49f91403b7042f2d64e470dd22
iliasgws <i.mouhcine@greenwoodschool.education>
2026-09-20 16:19:53 +0000

    Préparer la bêta 9

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "school.greenwood.plus"
         minSdk = 26
         targetSdk = 36
-        versionCode = 15
-        versionName = "0.5.0-beta.8"
+        versionCode = 16
+        versionName = "0.5.0-beta.9"
     }
 
     buildTypes {
